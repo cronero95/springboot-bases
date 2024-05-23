d13043501398c7216debdabfa666808ff3a17331
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-23 16:15:07 -0500

    feat: add method to find by name to HeroRepository

--- src/main/java/com/springbootprojects/myfirstproject/hero/HeroRepository.java
+++ src/main/java/com/springbootprojects/myfirstproject/hero/HeroRepository.java
@@ -1,5 +1,9 @@
 package com.springbootprojects.myfirstproject.hero;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HeroRepository extends JpaRepository<Hero, Integer> {}
+public interface HeroRepository extends JpaRepository<Hero, Integer> {
+    List<Hero> findAllByNameContaining(String name);
+}
