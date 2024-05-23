3dd67bddf3bd7ab7ac918ead5d4961b51a8d8653
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-23 15:46:34 -0500

    test: get heroResponseDto list from findAll method

--- src/test/java/com/springbootprojects/myfirstproject/hero/HeroServiceTest.java
+++ src/test/java/com/springbootprojects/myfirstproject/hero/HeroServiceTest.java
@@ -1,8 +1,12 @@
 package com.springbootprojects.myfirstproject.hero;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -67,5 +71,41 @@ public class HeroServiceTest {
 
         Mockito.verify(heroMapper, Mockito.times(1))
             .heroToDtoResponse(savedHero);
+    }
+
+    @Test
+    public void should_return_all_heroes() {
+
+        Hero hero_01 = new Hero();
+        hero_01.setName("Batman");
+        hero_01.setCity("Gotham City");
+        hero_01.setId(1);
+
+        Hero hero_02 = new Hero();
+        hero_02.setName("Wonder Woman");
+        hero_02.setCity("Themyscira");
+        hero_02.setId(2);
+
+        Hero hero_03 = new Hero();
+        hero_03.setName("Aquaman");
+        hero_03.setCity("Atlantis");
+        hero_03.setId(3);
+
+        List<Hero> heroes = new ArrayList<>();
+        heroes.add(hero_01);
+        heroes.add(hero_02);
+        heroes.add(hero_03);
+
+        Mockito.when(heroRepository.findAll())
+            .thenReturn(heroes);
+
+        Mockito.when(heroMapper.heroToDtoResponse(ArgumentMatchers.any(Hero.class)))
+            .thenReturn(new HeroResponseDto("any", "any"));
+
+        List<HeroResponseDto> heroResponseDtos = heroService.findAllHeroes();
+
+        Assertions.assertEquals(heroes.size(), heroResponseDtos.size());
+        Mockito.verify(heroRepository, Mockito.times(1))
+            .findAll();
     }
 }
